bc5b6043ff28c58c6f58c7f9c93ba097d7561d9b
Andrew Kozey <kozeyandrey@gmail.com>
2018-05-04 09:56:23 +0300

    Added getFirst to the Feature service

--- src/main/java/service/Feature.java
+++ src/main/java/service/Feature.java
@@ -1,20 +1,64 @@
 package service;
 
+import entity.Product;
+import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Feature {
-    private final Pattern featurePattern = Pattern.compile("\\d+[\\.]?\\d*");
+    private final DecimalFormat decimalFormat;
+    private final Product product;
+    private final HashMap<Double, Integer> features;
+    private Pattern featurePattern;
 
-    public HashMap<Double, Integer> get(String name) {
-        Matcher matcher = featurePattern.matcher(name);
-        HashMap<Double, Integer> features = new HashMap<>();
+    public Feature(Product product) {
+        this.product = product;
+        this.decimalFormat = new DecimalFormat("0.#");
+        this.features = new HashMap<>();
+    }
 
-        while (matcher.find()) {
-            features.merge(Double.valueOf(matcher.group()), 1, Integer::sum);
+    public HashMap<Double, Integer> getAll() {
+        featurePattern = Pattern.compile("\\d*[.]\\d+|\\d+");
+
+        for (String name : product.getNames()) {
+            Matcher matcher = featurePattern.matcher(name);
+
+            while (matcher.find()) {
+                features.merge(Double.valueOf(matcher.group()), 1, Integer::sum);
+            }
         }
 
         return features;
+    }
+
+    public String getFirst() {
+        String result = null;
+
+        if (features.isEmpty()) {
+            features.putAll(getAll());
+        }
+
+        Double feature = Collections.max(features.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+
+        if (feature % 1 == 0) {
+            featurePattern = Pattern.compile(decimalFormat.format(feature) + "+\\D?[a-zA-Z]+");
+        } else {
+            featurePattern = Pattern.compile(feature + "+\\D?[a-zA-Z]+");
+        }
+
+        for (String name : product.getNames()) {
+            Matcher matcher = featurePattern.matcher(name);
+
+            if (matcher.find()) {
+                result = matcher.group();
+                break;
+            }
+        }
+
+        return result;
     }
 }
