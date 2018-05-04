15f830a129ae418affc3f2060dfb596e6cab3ec2
Andrew Kozey <kozeyandrey@gmail.com>
2018-05-04 11:39:55 +0300

    Created Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import entity.Product;
+import entity.Result;
+import org.flywaydb.core.Flyway;
+import repository.ProductRepository;
+import repository.ResultRepository;
+import service.Search;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Flyway flyway = new Flyway();
+        HikariConfig cfg = new HikariConfig("./src/main/resources/db.properties");
+        HikariDataSource dataSource = new HikariDataSource(cfg);
+
+        flyway.setDataSource(dataSource);
+        flyway.migrate();
+
+        ProductRepository productRepository = new ProductRepository(dataSource);
+        ResultRepository resultRepository = new ResultRepository(dataSource);
+
+        List<Product> products = productRepository.getUnchecked();
+
+        for (Product product : products) {
+            Search search = new Search(product);
+            Result result = search.getResult();
+            resultRepository.create(result);
+
+            //TODO: Add batch
+            product.setChecked(true);
+            productRepository.update(product);
+        }
+    }
+}
